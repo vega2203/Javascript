61ad8789b27342ecb7a4cdc1dec8c0d329ed8f32
vega2203 <vega2203@users.noreply.github.com>
2019-12-17 20:55:14 -0800

    back to top button

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,36 @@ console.log(recipe);
 
 // Boolean
 // everything with a real value is true 0, null, undefined is false
+// comparison operators always return true or false ==, ===, !=, !==, !===, <, >, <=, >=, && AND, || OR, ! NOT
+const present = true;
+const absent = false;
+Boolean(present);
+console.log(present);
+// Undefined
+var cat = 
+console.log(cat);
+// Null
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+person = null;    // Now value is null, but type is still an object
+console.log(person);
+//Get the button
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+document.write("Hello World!");
+// command question mark is a single line comment
+
+var favMovie = 'My favorite movie is Dirty Dancing';
+document.write (favMovie)
